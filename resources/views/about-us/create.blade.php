901305c510dfcc2d33fd1b143b033a25c12a34a1
unknown <zaenalblaze@gmail.com>
2021-09-28 17:05:11 +0700

    about us

--- resources/views/about-us/create.blade.php
+++ resources/views/about-us/create.blade.php
@@ -1,40 +1,58 @@
 @extends('layouts.app')
 
-@section('title', 'About US')
+@section('title')
+    Edit About Us
+@endsection
 
 @section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Container-->
+    <div class="container-xxl" id="kt_content_container">
+        <!--begin::Navbar-->
 
-            <!--begin::Entry-->
-            <div class="d-flex flex-column-fluid">
-                <!--begin::Container-->
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <!--begin::Card-->
-                            <div class="card card-custom gutter-b example example-compact">
-                                <!--begin::Form-->
-                                <form action="{{ route("admin.about-us.post") }}" method="POST">
-                                    <div class="card-body">
-                                        <div class="form-group row">
-                                            <label class="col-2 col-form-label">About Us</label>
-                                            <div class="col-10">
-                                               <textarea class="form-control" rows="20" name="description" >{{ $aboutUs->description ?? "" }}</textarea>
-                                            </div>
-                                        </div>
-                                        
-                                    <div class="form-group float-right">
-                                        @csrf
-                                        <a href="{{ route("admin.about-us") }}" class="btn btn-danger">Cancel</a>
-                                        <button type="submit" class="btn btn-primary px-5">Edit</button>
-                                    </div>
-                                    
-                                    </div>
-                                </form>
-                            </div>
-                            <!--end::Card-->
-                    </div>
-                </div>
-                <!--end::Container-->
+        <!--end::Navbar-->
+        <!--begin::Basic info-->
+        <div class="card mb-5 mb-xl-10">
+            <!--begin::Card header-->
+            <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
+               
             </div>
-      
+            <!--begin::Card header-->
+            <!--begin::Content-->
+            <div id="kt_account_profile_details" class="collapse show">
+                <!--begin::Form-->
+                <form id="kt_account_profile_details_form" class="form" method="POST" enctype="multipart/form-data" action="{{ route("admin.about-us.post") }}">
+                    <!--begin::Card body-->
+                    <div class="card-body border-top p-9">
+                        <!--begin::Input group-->
+                        <div class="row mb-6">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 col-form-label required fw-bold fs-6">About Us</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8 fv-row">
+                                <textarea class="form-control" rows="20" name="description" >{{ $aboutUs->description ?? "" }}</textarea>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Input group-->
+                    </div>
+                    <!--end::Card body-->
+                    <!--begin::Actions-->
+                    <div class="card-footer d-flex justify-content-end py-6 px-9">
+                        @csrf
+                        <a href="{{ route("admin.about-us") }}" class="btn btn-danger btn-active-light-primary me-2">Cancel</a>
+                        <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">Edit</button>
+                    </div>
+                    <!--end::Actions-->
+                </form>
+                <!--end::Form-->
+            </div>
+            <!--end::Content-->
+        </div>
+        <!--end::Basic info-->
+    </div>
+    <!--end::Container-->
+</div>
 @endsection
+
--- resources/views/about-us/index.blade.php
+++ resources/views/about-us/index.blade.php
@@ -1,36 +1,48 @@
 @extends('layouts.app')
 
-@section('title', 'About US')
+@section('title')
+    About Us
+@endsection
 
 @section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Container-->
+    <div class="container-xxl" id="kt_content_container">
+        <!--begin::Navbar-->
 
-            <!--begin::Entry-->
-            <div class="d-flex flex-column-fluid">
-                <!--begin::Container-->
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <!--begin::Card-->
-                            <div class="card card-custom gutter-b example example-compact">
-                                <!--begin::Form-->
-                                    <div class="card-body">
-                                        <div class="form-group row">
-                                            <div class="col-12">
-                                               <textarea class="form-control" rows="20" disabled >{{ $aboutUs->description ?? "" }}</textarea>
-                                            </div>
-                                        </div>
-                                        
-                                        
-                                    <div class="form-group float-right">
-                                        <a href="{{ route("admin.about-us.create") }}" class="btn btn-primary px-5">Edit</a>
-                                    </div>
-                                    
-                                    </div>
-                            </div>
-                            <!--end::Card-->
-                    </div>
-                </div>
-                <!--end::Container-->
+        <!--end::Navbar-->
+        <!--begin::Basic info-->
+        <div class="card mb-5 mb-xl-10">
+            <!--begin::Card header-->
+            <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
+                <!--begin::Card title-->
+                <!--end::Card title-->
             </div>
-      
+            <!--begin::Card header-->
+            <!--begin::Content-->
+            <div id="kt_account_profile_details" class="collapse show">
+                <!--begin::Form-->
+                    <!--begin::Card body-->
+                    <div class="card-body border-top p-9">
+                        <div class="row mb-6">
+                            <label class="col-lg-4 col-form-label fw-bold fs-6">About Us</label>
+                            <div class="col-lg-8 fv-row">
+                                <textarea class="form-control form-control-lg form-control-solid" rows="20" disabled >{{ $aboutUs->description ?? "" }}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end::Card body-->
+                    <!--begin::Actions-->
+                    <div class="card-footer d-flex justify-content-end py-6 px-9">
+                        <a href="{{ route("admin.about-us.create") }}" class="btn btn-primary px-5">Edit</a>
+                    </div>
+                    <!--end::Actions-->
+                <!--end::Form-->
+            </div>
+            <!--end::Content-->
+        </div>
+        <!--end::Basic info-->
+    </div>
+    <!--end::Container-->
+</div>
 @endsection
